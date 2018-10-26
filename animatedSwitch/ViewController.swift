f4c2e5f2e1704686e50cf9a4bb4ef8ffb8b1cc69
gme_2 <gme_2@gme-2s-iMac.local>
2018-10-26 11:46:19 +0900

    switch color configured

--- animatedSwitch/ViewController.swift
+++ animatedSwitch/ViewController.swift
@@ -11,29 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var headingTitlelabel: UILabel!
-    @IBOutlet weak var headingTitleLabel2: UILabel!
     @IBOutlet weak var switch1: UISwitch!
-    @IBOutlet weak var switch2: UISwitch!
-
-    @IBOutlet weak var backGroundLayer2: UIView!
-    
     @IBOutlet weak var backgroundLayer: UIView!
     let containerView = UIView()
-    let containerView2 = UIView()
     var circleView: UIView?
-    var circleView2: UIView?
+    let hightlightColor = UIColor.init(rgb: 0x89B73B)
+    let siwtchOffStateColor = UIColor.lightGray
+    let hightlightHeadingColor = UIColor.init(rgb: 0x10ABFE)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.backgroundLayer.clipsToBounds = true
-        self.backGroundLayer2.clipsToBounds = true
         self.containerView.frame = self.backgroundLayer.frame
-        self.containerView2.frame = self.backGroundLayer2.frame
         self.backgroundLayer.insertSubview(containerView, at: 0)
-        self.backGroundLayer2.insertSubview(containerView2, at: 0)
         self.switch1.setOn(false, animated: false)
-        self.switch2.setOn(false, animated: false)
+        self.switch1.onTintColor = UIColor.init(rgb: 0x739E66)
+        self.switch1.tintColor = UIColor.lightGray
     }
     
 
@@ -41,9 +35,7 @@ class ViewController: UIViewController {
         animate(from: self.switch1.center, expanding: sender.isOn)
     }
     
-    @IBAction func switch2(_ sender: UISwitch) {
-        animate2(from: self.switch2.center, expanding: sender.isOn)
-    }
+
     
     func animate(from point: CGPoint?, expanding: Bool) {
         guard let center = point else {
@@ -54,18 +46,16 @@ class ViewController: UIViewController {
         
         circleView?.center =  center
         circleView?.layer.cornerRadius = (circleView?.bounds.width)!/2
-        circleView?.backgroundColor = UIColor.red
+        circleView?.backgroundColor = hightlightColor
         self.containerView.addSubview(circleView!)
 
         let p = self.view.bounds.width
         let b = self.view.bounds.height
         let rSquare = pow(p, 2) + pow(b, 2)
         let r = sqrt(rSquare)/6
-        //
-        
         
         let animator = UIViewPropertyAnimator(duration: 0.4, curve: UIViewAnimationCurve.easeInOut)
-        
+
         if expanding {
             animator.addAnimations {
                 self.circleView?.transform =  CGAffineTransform.init(scaleX: r, y: r)
@@ -76,62 +66,31 @@ class ViewController: UIViewController {
             }
         }
         
-        
-        animator.addCompletion { (_) in
-//            self.backgroundLayer.backgroundColor = UIColor.red
-//            self.circleView?.removeFromSuperview()
-        }
-        
-        let blueColor = UIColor.init(red: 0, green: 169, blue: 209, alpha: 1)
-        
+        let blueColor = hightlightHeadingColor
         UIView.transition(with: self.headingTitlelabel, duration: 0.2, options: [UIViewAnimationOptions.transitionCrossDissolve, .curveEaseIn], animations: {
             self.headingTitlelabel.textColor =  expanding ? .white : blueColor
         }, completion: nil)
         
         animator.startAnimation()
     }
-    
-    
-
-        func animate2(from point: CGPoint?, expanding: Bool) {
-            guard let center = point else {
-                return
-            }
-            
-            self.circleView2 = self.circleView2 ??  UIView(frame: CGRect.init(x: 0, y: 0, width: 5, height: 5))
-            circleView2?.center =  center
-            circleView2?.layer.cornerRadius = (circleView2?.bounds.width)!/2
-            circleView2?.backgroundColor = UIColor.red
-            self.containerView2.addSubview(circleView2!)
-            let p = self.view.bounds.width
-            let b = self.view.bounds.height
-            let rSquare = pow(p, 2) + pow(b, 2)
-            let r = sqrt(rSquare)/6
-
-            let animator = UIViewPropertyAnimator(duration: 0.4, curve: UIViewAnimationCurve.easeInOut)
-            
-            if expanding {
-                animator.addAnimations {
-                    self.circleView2?.transform =  CGAffineTransform.init(scaleX: r, y: r)
-                }
-            }else {
-                animator.addAnimations {
-                    self.circleView2?.transform =  CGAffineTransform.identity
-                }
-            }
-            
-            let blueColor = UIColor.init(red: 0, green: 169, blue: 209, alpha: 1)
-            
-            UIView.transition(with: self.headingTitleLabel2, duration: 0.2, options: [UIViewAnimationOptions.transitionCrossDissolve, .curveEaseIn], animations: {
-                self.headingTitleLabel2.textColor =  expanding ? .white : blueColor
-            }, completion: nil)
-            
-            animator.startAnimation()
-            
-            
-        }
-        
-    
-    
 }
 
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
